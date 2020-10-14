433f888139eead75ec35fbd4b38add624f173ad4
MatzeK105 <klaias@web.de>
2020-10-14 18:15:14 +0200

    Implemented base functionalies; List, Items

---
+++ webapp/sdf/src/components/Item.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h2>{{title}} ({{votes}})</h2>
+    <button>Upvote</button>
+    <button>Downvote</button>
+    <button v-on:click="removeItem(title)">Remove</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Item',
+  props: {
+    title: String,
+    votes: Number
+  },
+  methods: {
+    removeItem: function(_title) {
+        this.$emit('removeEvent', _title)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
