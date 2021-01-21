0ff73821d1ca82b21ba8b947dbb0dcf7a3373ba4
Sboui431 <nidhal.sboui52@gmail.com>
2021-01-21 13:45:33 +0100

    Pairprogramming: Implemented Login

--- webapp/components/login-form-component/login-form-component.vue
+++ webapp/components/login-form-component/login-form-component.vue
@@ -1,16 +1,12 @@
 <template>
-  <div>
-    <form>
-      <div>
-        <label for="username">Username</label>
-        <br>
-        <input id="username" type="text" />
-      </div>
-      <div>
-        <label for="password">Password</label>
-        <br>
-        <input id="password" type="password" />
-      </div>
+  <div class="container">
+    <form class="sign-in">
+      <h2> Sign In </h2>
+      <div>Use your account</div>
+        <input class="input_sign" id="username" type="text" placeholder="Username" />
+        <input class="input_sign" id="password" type="password"  placeholder="Passowrd"/>
+        <input class="button_sign" type="submit" value="Login" name="login" id="login" />
+      
     </form>
   </div>
 </template>
@@ -20,6 +16,13 @@ import Index from "../../pages/index.vue";
 
 export default {
   name: "LoginFormComponent",
+  data(){
+    return{
+      username:'',
+      password:''
+    };
+
+  },
   components: {
     Index,
   },
@@ -27,8 +30,62 @@ export default {
 </script>
 
 <style>
-label{
-  text-align: left;
+.container{
+  position: relative;
+  left: 30%;
+  width: 500px;
+  height: 400px;
+  border-radius: 10px;
+  overflow: hidden;
+  box-shadow: 0 15px 30px rgba(0, 0, 0, .2),
+              0 15px 30px rgba(0, 0, 0, .2);
+  background: linear-gradient(to bottom , #efefef, #ccc);
 
 }
+h2{
+  margin: 0;
+}
+.sign-in{
+  position: absolute;
+  left: 25%;
+  top: 20%;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: column;
+  padding: 20% 10%;
+  width: calc(70% - 10%);
+  height: calc(70% - 20%);
+  text-align: center;
+  background: linear-gradient(to bottom , #efefef , #ccc);
+  transition: all .5s ease-in-out;
+}
+
+.input_sign {
+  background-color: #eee;
+  border: none;
+  padding: 8px 15px;
+  margin: 6px 0;
+  width: calc(150% - 10%);
+  border-radius: 15px;
+  box-shadow: inset 0 1px 2px rgb(0, 0, 0 , .4),
+                    0 -1px 1px #fff,
+                    0 1px 0 #fff;
+  overflow: hidden;
+
+  
+  }
+.button_sign{
+  border-radius: 20px;
+  border: 1px solid #009345;
+  background-color:#009345 ;
+  color: #fff;
+  font-size: 1rem;
+  
+  padding: 10px 40px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  transition: transform .1s ease-in;      
+}
+
 </style>
