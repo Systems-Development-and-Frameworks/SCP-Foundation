059e48235d3f698b4e71f5e23c01994550019862
Dominik Wegner <dominik.wegner@navitas.de>
2021-01-26 18:55:47 +0100

    fixed lighthouse

--- webapp/nuxt.config.js
+++ webapp/nuxt.config.js
@@ -8,7 +8,7 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/icon.png' }
     ]
   },
 
@@ -53,7 +53,9 @@ export default {
 
   pwa: {
     manifest: {
-      icon: false
+      name: 'SCP-Foundation',
+      lang: 'en',
+      start_url: '/',
     }
   }
 }
